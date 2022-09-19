fd0ae810f84a181a4294a3cc770aa1b73db7f057
Marcelo_borges21 <marceloba43212gmail.com>
2022-09-19 10:29:58 -0300

    Aprendo a integração da api no projeto.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,37 @@
 import React from 'react';
+
+import CONST from '../data/constants';
 import  {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
 import {faPlay, faPlus} from '@fortawesome/free-solid-svg-icons'
 
+import Score from './Score';
+
 import placeholder from '../assets/home1.png'
 
-const Hero = ({ title = 'Avengers Endgame', score = 10 }) => {
-   const getBorderColor = () =>{
-      if (score >= 7) {
-         return 'border-green-400';
-      }else if (score > 4 && score > 7){
-         return 'border-yellow-400';
-      } else {
-         return 'border-red-400';
-      }
-   } 
 
+const Hero = ({ backdrop_path = null, title = 'Avengers Endgame', vote_average = 10 }) => {
+   const { IMAGEURL } = CONST;
    return (
-     <header className='box-border relative min-h-screen -mb-32 '>
-        <img className='object-cover object-center h-auto w-full' src={placeholder} alt="Filme em destaque" />
-        <div className='absolute left-0 bottom-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
-        <article className='absolute bottom-0 mb-64 px-8'>
-         <p className='text-3xl '>Assista agora:</p>
-         <h2 className='text-6xl font-black'>{title}</h2>
-         <p className='text-base'>Nota <span className={`inline-block mx-2 py-1 px-2  border-4 bg-black bg-opacity-75 rounded-full 
-            ${getBorderColor()}`}>{score } </span>
-         </p>
-         <button className='text-base py-2  px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-500 
-         ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
-            <FontAwesomeIcon className='mr-2' icon={faPlay} /> Assistir 
-         </button>
-         <button className='text-base py-2  px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-500 
-         ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
-            <FontAwesomeIcon className='mr-2' icon={faPlus} /> Minha Lista 
-         </button>
-        </article>
-     </header>
-
+   <header className='box-border relative min-h-screen -mb-32 '>
+      <img className='object-cover object-center h-auto w-full' src={backdrop_path ? `${IMAGEURL}/original/${backdrop_path}` : placeholder} alt="Filme em destaque" />
+      <div className='absolute left-0 bottom-0 w-full h-full bg-gradient-to-b from-transparent to-black'></div>
+      <article className='absolute bottom-0 mb-64 px-8'>
+      <p className='text-3xl '>Assista agora:</p>
+      <h2 className='text-6xl font-black'>{title}</h2>
+      <p className='text-base'>Nota{''}
+         <Score value={vote_average}/>
+      </p>
+      <button className='text-base py-2  px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-500 
+      ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+         <FontAwesomeIcon className='mr-2' icon={faPlay} /> Assistir 
+      </button>
+      <button className='text-base py-2  px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-500 
+      ease-in-out hover:bg-white hover:bg-opacity-75 hover:text-black'>
+         <FontAwesomeIcon className='mr-2' icon={faPlus} /> Minha Lista 
+      </button>
+      </article>
+   </header>
    )
-
-   };
+};
 
 export default Hero;
---
+++ src/components/Poster.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+
+import Score from "./Score";
+
+import { Movie } from "../data/mock";
+
+import './Poster.css';
+
+
+const Poster = ({ cover, title, score }:Movie, index: number) => (
+    <article className="relative transition-all duration-300 
+    ease-in-out transform hover:scale-110" key={index}>
+        <img src={cover} alt={title} />
+        <div className="poster cursor-pointer absolute inset-0 w-full h-ful py-8 px-2 grid place-items-center bg-black bg-opacity-75 transition-all duration-500 
+                ease-in-out opacity-0">
+            <FontAwesomeIcon icon={faPlayCircle} size='5x'/>
+            <h2 className="text-2xl">{title}</h2>
+            <Score value={score}/>
+            
+        </div>
+    </article>
+
+);
+
+export default Poster;
